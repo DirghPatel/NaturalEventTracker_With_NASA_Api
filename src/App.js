a03d3b7e18283fb79c7bbd77a0feb0828d6d82d8
Joyal Shah <63547847+joyal7701@users.noreply.github.com>
2021-10-02 16:24:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 import GoogleMap from './GoogleMap'
-import { useState , useEffect } from 'react'
 import styled from 'styled-components'
 
 function App() {
